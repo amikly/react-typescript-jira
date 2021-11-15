51c5f84e3e764819da1a259fd8ef4a6a9205674b
amikly <lykamin@yahoo.com>
2021-11-16 01:34:49 +0800

    feat: 添加中间件middleware.js

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,43 @@
+import React, { FormEvent } from "react";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const LoginScreen = () => {
+  const login = (param: { username: string; password: string }) => {
+    fetch(`${apiUrl}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then(async (response: Response) => {
+      if (response.ok) {
+      }
+    });
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    // 阻止表单的默认行为（页面的跳转与刷新）
+    event.preventDefault();
+    const username = (event.currentTarget.elements[0] as HTMLInputElement)
+      .value;
+    const password = (event.currentTarget.elements[1] as HTMLInputElement)
+      .value;
+    login({ username, password });
+  };
+
+  return (
+    // onSubmit绑定点击type为submit的按钮时发生的事件
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">用户名</label>
+        <input type="text" id="username" />
+      </div>
+      <div>
+        <label htmlFor="password">密码</label>
+        <input type="password" id="password" />
+      </div>
+      <button type="submit">登录</button>
+    </form>
+  );
+};
